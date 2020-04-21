be02bb0d1a64c2608e949322faa60a37e87d0269
Juan Miguel Gomez <juanmiguelgomez8@gmail.com>
2020-04-21 09:01:07 -0500

    Fixing repo

---
+++ packages/_atoms/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./App.css"
+
+function App() {
+  return (
+    <div>
+      <button className="eds-button">Endava button</button>
+    </div>
+  );
+}
+
+export default App;
